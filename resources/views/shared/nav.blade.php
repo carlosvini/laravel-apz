f67272e22617a407b5a808224c5db8745708a990
Carlos Rodrigues <carlos@jp7.com.br>
2015-07-20 14:03:27 +1000

    Add asynchronous external content

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -11,10 +11,13 @@
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+              <ul class="nav navbar-nav">
                 <li>{!! link_to_route('articles.index', 'Articles') !!}</li>
                 <li>{!! link_to_route('authors.index', 'Authors') !!}</li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+            <li id="weather" class="navbar-text">Loading weather...</li>
+           </ul>
         </div>
     </div>
 </nav>
